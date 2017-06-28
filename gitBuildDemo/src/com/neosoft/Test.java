d4677b458bf25b0692278cd98f74e8b6f3b8e099
user <user@AT-R6P12>
2017-06-28 15:57:15 +0530

    hiiii

--- gitBuildDemo/src/com/neosoft/Test.java
+++ gitBuildDemo/src/com/neosoft/Test.java
@@ -3,6 +3,6 @@ package com.neosoft;
 public class Test {
 	public static void main(String args[]) {
 
-		System.out.println("in git build demo3");
+		System.out.println("in git build demo4");
 	}
 }
